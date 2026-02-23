dc9a16e5c3a9a6f6df21557186b9058e9aef8707
Jason CheongKeeYou <jasonc@nulogy.com>
2026-02-23 08:22:39 -0500

    refactor

--- lib/v1/tennis.rb
+++ lib/v1/tennis.rb
@@ -3,69 +3,40 @@
 module TennisKata
   SCORE_NAMES = %w[Love Fifteen Thirty Forty].freeze
 
-  module TennisScoring
-    def initialize(player1_name, player2_name)
-      @player1_name = player1_name
-      @player2_name = player2_name
-      @p1_points = 0
-      @p2_points = 0
-    end
+  class Player < Struct.new(:name, :points)
+    def initialize(name) = super(name, 0)
 
-    def won_point(player_name)
-      if player_name == @player1_name
-        @p1_points += 1
-      else
-        @p2_points += 1
+    def win_point = self.points += 1
+
+    def to_s = name
+    def score = SCORE_NAMES[points]
+  end
+
+  class TennisGame < Struct.new(:player1, :player2)
+    def initialize(name1, name2) = super(Player.new(name1), Player.new(name2))
+
+    def won_point(player_name) = find_player(player_name).win_point
+
+    def score
+      case [max_points, point_diff]
+      in [3.., 0] then "Deuce"
+      in [..2, 0] then "#{leader.score}-All"
+      in [4.., 1] then "Advantage #{leader}"
+      in [4.., 2..] then "Win for #{leader}"
+      in [..3, 1..] then "#{player1.score}-#{player2.score}"
       end
     end
 
     private
 
-    def tied_score
-      @p1_points >= 3 ? "Deuce" : "#{SCORE_NAMES[@p1_points]}-All"
-    end
-
-    def advantage_or_win_score
-      diff = @p1_points - @p2_points
-      leader = diff > 0 ? @player1_name : @player2_name
-      diff.abs == 1 ? "Advantage #{leader}" : "Win for #{leader}"
-    end
-
-    def running_score
-      "#{SCORE_NAMES[@p1_points]}-#{SCORE_NAMES[@p2_points]}"
-    end
+    def find_player(name) = players.find { it.name == name }
+    def point_diff = players.map(&:points).reduce(&:-).abs
+    def max_points = players.map(&:points).max
+    def leader = players.max_by(&:points)
+    def players = [player1, player2]
   end
 
-  class TennisGame1
-    include TennisScoring
-
-    def score
-      return tied_score if @p1_points == @p2_points
-      return advantage_or_win_score if @p1_points >= 4 || @p2_points >= 4
-
-      running_score
-    end
-  end
-
-  class TennisGame2
-    include TennisScoring
-
-    def score
-      return tied_score if @p1_points == @p2_points
-      return advantage_or_win_score if @p1_points >= 4 || @p2_points >= 4
-
-      running_score
-    end
-  end
-
-  class TennisGame3
-    include TennisScoring
-
-    def score
-      return tied_score if @p1_points == @p2_points
-      return advantage_or_win_score if @p1_points >= 4 || @p2_points >= 4
-
-      running_score
-    end
-  end
+  class TennisGame1 < TennisGame; end
+  class TennisGame2 < TennisGame; end
+  class TennisGame3 < TennisGame; end
 end
