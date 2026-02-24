3dc23162df271e72c84748a652f0650ed81dd97a
Jason CheongKeeYou <jasonc@nulogy.com>
2026-02-23 19:59:42 -0500

    v1: character_creator

--- lib/v1/character_creator.rb
+++ lib/v1/character_creator.rb
@@ -1,9 +1,5 @@
 module CharacterCreatorKata
-  def get_input(default)
-    puts default
-    default
-    # gets
-  end
+  # --- Constants ---
 
   module Proficiencies
     ATHLETICS = "athletics"
@@ -26,6 +22,15 @@ module CharacterCreatorKata
     PERSUASION = "persuasion"
   end
 
+  ALL_SKILLS = [
+    Proficiencies::ATHLETICS, Proficiencies::ACROBATICS, Proficiencies::SLEIGHT_OF_HAND,
+    Proficiencies::STEALTH, Proficiencies::ARCANA, Proficiencies::HISTORY,
+    Proficiencies::INVESTIGATION, Proficiencies::NATURE, Proficiencies::RELIGION,
+    Proficiencies::ANIMAL_HANDLING, Proficiencies::INSIGHT, Proficiencies::MEDICINE,
+    Proficiencies::PERCEPTION, Proficiencies::SURVIVAL, Proficiencies::DECEPTION,
+    Proficiencies::INTIMIDATION, Proficiencies::PERFORMANCE, Proficiencies::PERSUASION
+  ].freeze
+
   HIT_DICE = {
     "Barbarian" => 12,
     "Fighter" => 10, "Paladin" => 10, "Ranger" => 10,
@@ -33,28 +38,11 @@ module CharacterCreatorKata
     "Sorcerer" => 6, "Wizard" => 6
   }.freeze
 
-  def hitpointcalculator(char_class, level, con)
-    die = HIT_DICE[char_class]
-    die + con + (level * (die / 2 + 1 + con))
-  end
+  CLASSES = %w[Barbarian Bard Cleric Druid Fighter Monk Paladin Ranger Rogue Sorcerer Warlock Wizard].freeze
 
-  def pick_from_list(options, prompt)
-    puts options
-    puts prompt
-    choice = ""
-    until options.include?(choice)
-      choice = get_input(options.first).split(/ |\_|\-/).map(&:capitalize).join(" ").chomp
-      puts "please select an option from the list" unless options.include?(choice)
-    end
-    choice
-  end
+  SPECIES = %w[Dragonborn Dwarf Elf Gnome Goliath Halfling Human Orc Tiefling].freeze
 
-  def backgroundpick
-    pick_from_list(
-      ["Acolyte", "Criminal", "Sage", "Soldier"],
-      "What species is your character? "
-    )
-  end
+  BACKGROUNDS = %w[Acolyte Criminal Sage Soldier].freeze
 
   BACKGROUND_BONUSES = {
     "Acolyte" => {
@@ -77,66 +65,6 @@ module CharacterCreatorKata
 
   DEFAULT_BACKGROUND_BONUS = { ability_bonuses: [], skill_proficiencies: [] }.freeze
 
-  def background_bonuses(background)
-    BACKGROUND_BONUSES.fetch(background, DEFAULT_BACKGROUND_BONUS)
-  end
-
-  class Character
-    attr_accessor :charname, :level, :species, :class_of_char, :background,
-                  :str, :strmod, :dex, :dexmod, :con, :conmod,
-                  :int, :intmod, :wis, :wismod, :cha, :chamod,
-                  :ac, :prof, :hitpoints, :skills
-
-    def initialize(charname:, level:, species:, class_of_char:, background:,
-                   str:, strmod:, dex:, dexmod:, con:, conmod:,
-                   int:, intmod:, wis:, wismod:, cha:, chamod:,
-                   ac:, prof:, hitpoints:, skills:)
-      @charname = charname
-      @level = level
-      @species = species
-      @class_of_char = class_of_char
-      @background = background
-      @str = str
-      @strmod = strmod
-      @dex = dex
-      @dexmod = dexmod
-      @con = con
-      @conmod = conmod
-      @int = int
-      @intmod = intmod
-      @wis = wis
-      @wismod = wismod
-      @cha = cha
-      @chamod = chamod
-      @ac = ac
-      @prof = prof
-      @hitpoints = hitpoints
-      @skills = skills
-    end
-
-    def context
-      instance_variables.map do |attribute|
-        { attribute => instance_variable_get(attribute) }
-      end
-    end
-  end
-
-  def classpick
-    pick_from_list(
-      ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue",
-       "Sorcerer", "Warlock", "Wizard"],
-      "What class is your character? "
-    )
-  end
-
-  def my_modcalc(stat)
-    (stat - 10) / 2
-  end
-
-  def proficiency(level)
-    (level - 1) / 4 + 2
-  end
-
   CLASS_PROFICIENCIES = {
     "Barbarian" => %w[animal\ handling athletics intimidation nature perception survival],
     "Bard"      => %w[athletics acrobatics sleight\ of\ hand stealth arcana history investigation
@@ -154,98 +82,173 @@ module CharacterCreatorKata
     "Wizard"    => %w[arcana history insight investigation medicine nature religion]
   }.freeze
 
-  def proficiency_by_class(character_class)
+  STAT_NAMES = %w[Strength Dexterity Constitution Intelligence Wisdom Charisma].freeze
+
+  STANDARD_ARRAY = [15, 14, 13, 12, 10, 8].freeze
+
+  # --- Value Objects ---
+
+  class AbilityScores
+    ABILITY_NAMES = %i[str dex con int wis cha].freeze
+
+    attr_reader :str, :dex, :con, :int, :wis, :cha
+
+    def initialize(str:, dex:, con:, int:, wis:, cha:)
+      @str = str
+      @dex = dex
+      @con = con
+      @int = int
+      @wis = wis
+      @cha = cha
+    end
+
+    def str_mod = modifier(str)
+    def dex_mod = modifier(dex)
+    def con_mod = modifier(con)
+    def int_mod = modifier(int)
+    def wis_mod = modifier(wis)
+    def cha_mod = modifier(cha)
+
+    def to_h
+      {
+        str: str, str_mod: str_mod,
+        dex: dex, dex_mod: dex_mod,
+        con: con, con_mod: con_mod,
+        int: int, int_mod: int_mod,
+        wis: wis, wis_mod: wis_mod,
+        cha: cha, cha_mod: cha_mod
+      }
+    end
+
+    private
+
+    def modifier(score)
+      (score - 10) / 2
+    end
+  end
+
+  class Character
+    attr_reader :name, :level, :species, :char_class, :background,
+                :ability_scores, :ac, :proficiency_bonus, :hit_points, :skills
+
+    def initialize(name:, level:, species:, char_class:, background:,
+                   ability_scores:, ac:, proficiency_bonus:, hit_points:, skills:)
+      @name = name
+      @level = level
+      @species = species
+      @char_class = char_class
+      @background = background
+      @ability_scores = ability_scores
+      @ac = ac
+      @proficiency_bonus = proficiency_bonus
+      @hit_points = hit_points
+      @skills = skills
+    end
+
+    def to_h
+      {
+        name: name,
+        level: level,
+        species: species,
+        char_class: char_class,
+        background: background,
+        **ability_scores.to_h,
+        ac: ac,
+        proficiency_bonus: proficiency_bonus,
+        hit_points: hit_points,
+        skills: skills
+      }
+    end
+  end
+
+  # --- Calculations ---
+
+  def self.ability_modifier(score)
+    (score - 10) / 2
+  end
+
+  def self.proficiency_bonus(level)
+    (level - 1) / 4 + 2
+  end
+
+  def self.calculate_hit_points(char_class, level, con_mod)
+    die = HIT_DICE[char_class]
+    die + con_mod + (level * (die / 2 + 1 + con_mod))
+  end
+
+  def self.background_bonuses(background)
+    BACKGROUND_BONUSES.fetch(background, DEFAULT_BACKGROUND_BONUS)
+  end
+
+  def self.proficiency_by_class(character_class)
     CLASS_PROFICIENCIES.fetch(character_class, [])
   end
 
-
-  def pick_one_proficiency(available_skills, prof_list, slot, prompt)
-    puts prompt
-    while prof_list[slot].nil?
-      choice = get_input(prof_list.compact.first).chomp
-      puts choice if slot == 19
-      if available_skills.include?(choice)
-        prof_list[slot] = choice
-        available_skills.delete_at(available_skills.index(choice))
-      else
-        puts "please select one of the offered skills (case sensitive)"
-      end
-    end
-  end
-
-  def profpicker
-    available_skills = %w[athletics acrobatics sleight_of_hand stealth arcana history investigation nature religion
-                          animal_handling insight medicine perception survival deception intimidation performance
-                          persuasion]
-
-    puts "These are your stats: "
-    (0...19).each { |n| puts $prof[n] }
-
-    prompts = [
-      "Which skill would you like to choose as your first proficiency (case sensitive)",
-      "Which skill would you like to choose as your second proficiency (case sensitive)",
-      "Which skill would you like to choose as your second proficiency (case sensitive)"
-    ]
-
-    [19, 20, 21].each_with_index do |slot, i|
-      pick_one_proficiency(available_skills, $prof, slot, prompts[i])
-
-      if i < 2
-        puts "These are your remaining stats: "
-        puts available_skills
-      end
-    end
-  end
-
-
-  def speciespick
-    pick_from_list(
-      ["Dragonborn", "Dwarf", "Elf", "Gnome", "Goliath", "Halfling",
-       "Human", "Orc", "Tiefling"],
-      "What species is your character? "
-    )
-  end
-
-  ALL_SKILLS = [
-    Proficiencies::ATHLETICS, Proficiencies::ACROBATICS, Proficiencies::SLEIGHT_OF_HAND,
-    Proficiencies::STEALTH, Proficiencies::ARCANA, Proficiencies::HISTORY,
-    Proficiencies::INVESTIGATION, Proficiencies::NATURE, Proficiencies::RELIGION,
-    Proficiencies::ANIMAL_HANDLING, Proficiencies::INSIGHT, Proficiencies::MEDICINE,
-    Proficiencies::PERCEPTION, Proficiencies::SURVIVAL, Proficiencies::DECEPTION,
-    Proficiencies::INTIMIDATION, Proficiencies::PERFORMANCE, Proficiencies::PERSUASION
-  ].freeze
-
-  def skillpopulator
+  def self.initialize_skills
     ALL_SKILLS.each_with_object({}) { |skill, hash| hash[skill] = 0 }
   end
 
+  def self.stat_roll
+    STANDARD_ARRAY.dup
+  end
 
-  STAT_NAMES = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom"].freeze
+  # --- Input Helpers ---
 
-  def pick_one_stat(stats, finalstats, index, stat_name)
+  def get_input(default)
+    puts default
+    default
+    # gets
+  end
+
+  def pick_from_list(options, prompt)
+    puts options
+    puts prompt
+    choice = ""
+    until options.include?(choice)
+      choice = get_input(options.first).split(/ |\_|\-/).map(&:capitalize).join(" ").chomp
+      puts "please select an option from the list" unless options.include?(choice)
+    end
+    choice
+  end
+
+  def pick_class
+    pick_from_list(CLASSES, "What class is your character? ")
+  end
+
+  def pick_species
+    pick_from_list(SPECIES, "What species is your character? ")
+  end
+
+  def pick_background
+    pick_from_list(BACKGROUNDS, "What background is your character? ")
+  end
+
+  def pick_one_stat(available, finalstats, index, stat_name)
     puts "Which number would you like to be your #{stat_name} stat? "
 
     while finalstats[index].nil?
-      value = get_input(stats.compact.first).to_i
-      if stats.include?(value)
+      value = get_input(available.compact.first).to_i
+      if available.include?(value)
         finalstats[index] = value
-        stats[stats.index(value)] = nil
+        available[available.index(value)] = nil
       else
         puts "Please select one of the available numbers"
       end
     end
   end
 
-  def statpick(stats)
+  def assign_stats(stats)
+    pickable = STAT_NAMES[0...-1] # All except Charisma (auto-assigned)
+
     loop do
       finalstats = []
       puts "These are your stats: "
       stats.each { |s| puts s }
 
-      STAT_NAMES.each_with_index do |name, index|
+      pickable.each_with_index do |name, index|
         pick_one_stat(stats, finalstats, index, name)
 
-        if index < STAT_NAMES.length - 1
+        if index < pickable.length - 1
           puts "These are your remaining stats: "
           stats.compact!
           puts stats
@@ -254,8 +257,9 @@ module CharacterCreatorKata
 
       finalstats[5] = stats.compact.first
 
-      puts "So your stats are  Strength: #{finalstats[0]}, Dexteriy: #{finalstats[1]}, Constitution: #{finalstats[2]},
-    Intelligence: #{finalstats[3]}, Wisdom: #{finalstats[4]}, and Charisma: #{finalstats[5]}"
+      puts "Your stats are: Strength: #{finalstats[0]}, Dexterity: #{finalstats[1]}, " \
+           "Constitution: #{finalstats[2]}, Intelligence: #{finalstats[3]}, " \
+           "Wisdom: #{finalstats[4]}, Charisma: #{finalstats[5]}"
 
       puts "Are you satisfied with this? (y/n)"
       finish = ""
@@ -275,55 +279,48 @@ module CharacterCreatorKata
     end
   end
 
-  def statroll
-    [15, 14, 13, 12, 10, 8]
-  end
+  # --- Main Orchestration ---
 
   def main
     puts "Welcome to the D&D Character Creator!"
     puts "=" * 40
 
-    charclass = classpick
-    species = speciespick
-    background = backgroundpick
+    char_class = pick_class
+    species = pick_species
+    background = pick_background
 
-    stats = statroll
+    stats = CharacterCreatorKata.stat_roll
     puts "You rolled: #{stats.inspect}"
-    finalstats = statpick(stats)
+    finalstats = assign_stats(stats)
 
     level = 1
     str, dex, con, int, wis, cha = finalstats
-    strmod = my_modcalc(str)
-    dexmod = my_modcalc(dex)
-    conmod = my_modcalc(con)
-    intmod = my_modcalc(int)
-    wismod = my_modcalc(wis)
-    chamod = my_modcalc(cha)
+    ability_scores = AbilityScores.new(str: str, dex: dex, con: con, int: int, wis: wis, cha: cha)
 
-    prof = proficiency(level)
-    hitpoints = hitpointcalculator(charclass, level, conmod)
-    skills = skillpopulator
+    prof = CharacterCreatorKata.proficiency_bonus(level)
+    hit_points = CharacterCreatorKata.calculate_hit_points(char_class, level, ability_scores.con_mod)
+    skills = CharacterCreatorKata.initialize_skills
 
     puts "\nWhat is your character's name?"
-    charname = get_input("Adventurer")
+    name = get_input("Adventurer")
 
-    $charstats = Character.new(
-      charname: charname, level: level, species: species, class_of_char: charclass, background: background,
-      str: str, strmod: strmod, dex: dex, dexmod: dexmod, con: con, conmod: conmod,
-      int: int, intmod: intmod, wis: wis, wismod: wismod, cha: cha, chamod: chamod,
-      ac: 10 + dexmod, prof: prof, hitpoints: hitpoints, skills: skills
+    character = Character.new(
+      name: name, level: level, species: species, char_class: char_class, background: background,
+      ability_scores: ability_scores, ac: 10 + ability_scores.dex_mod,
+      proficiency_bonus: prof, hit_points: hit_points, skills: skills
     )
 
     puts "\n" + "=" * 40
     puts "Character Created!"
     puts "=" * 40
-    puts $charstats.context
+    character.to_h.each { |key, value| puts "  #{key}: #{value}" }
+
+    character
   end
 end
 
 # Allow running this file directly for manual testing
 if __FILE__ == $0
   include CharacterCreatorKata
-
   main
 end
--- test/character_creator_test.rb
+++ test/character_creator_test.rb
@@ -3,135 +3,315 @@ require 'minitest/reporters'
 require 'stringio'
 require_relative '../lib/version_loader'
 VersionLoader.require_kata('character_creator')
-include CharacterCreatorKata
 
 Minitest::Reporters.use!
 
-class CharacterCreatorTest < Minitest::Test
-  def run_character_creation
-    main
+class AbilityScoresTest < Minitest::Test
+  def setup
+    @scores = CharacterCreatorKata::AbilityScores.new(
+      str: 15, dex: 14, con: 13, int: 12, wis: 10, cha: 8
+    )
   end
+
+  def test_stores_ability_scores
+    assert_equal 15, @scores.str
+    assert_equal 14, @scores.dex
+    assert_equal 13, @scores.con
+    assert_equal 12, @scores.int
+    assert_equal 10, @scores.wis
+    assert_equal 8,  @scores.cha
+  end
+
+  def test_computes_positive_modifiers
+    assert_equal 2, @scores.str_mod  # 15 → +2
+    assert_equal 2, @scores.dex_mod  # 14 → +2
+    assert_equal 1, @scores.con_mod  # 13 → +1
+    assert_equal 1, @scores.int_mod  # 12 → +1
+  end
+
+  def test_computes_zero_modifier
+    assert_equal 0, @scores.wis_mod  # 10 → 0
+  end
+
+  def test_computes_negative_modifier
+    assert_equal(-1, @scores.cha_mod)  # 8 → -1
+  end
+
+  def test_modifier_edge_cases
+    low = CharacterCreatorKata::AbilityScores.new(str: 1, dex: 3, con: 6, int: 9, wis: 10, cha: 11)
+    assert_equal(-5, low.str_mod)  # 1 → -5
+    assert_equal(-4, low.dex_mod)  # 3 → -4
+    assert_equal(-2, low.con_mod)  # 6 → -2
+    assert_equal(-1, low.int_mod)  # 9 → -1
+    assert_equal 0,  low.wis_mod   # 10 → 0
+    assert_equal 0,  low.cha_mod   # 11 → 0
+  end
+
+  def test_to_h_includes_scores_and_modifiers
+    h = @scores.to_h
+    assert_equal 15, h[:str]
+    assert_equal 2,  h[:str_mod]
+    assert_equal 8,  h[:cha]
+    assert_equal(-1, h[:cha_mod])
+  end
+end
+
+class AbilityModifierTest < Minitest::Test
+  def test_standard_modifier_values
+    expected = {
+      1 => -5, 2 => -4, 3 => -4, 4 => -3, 5 => -3,
+      6 => -2, 7 => -2, 8 => -1, 9 => -1, 10 => 0,
+      11 => 0, 12 => 1, 13 => 1, 14 => 2, 15 => 2,
+      16 => 3, 17 => 3, 18 => 4, 19 => 4, 20 => 5
+    }
+    expected.each do |score, mod|
+      assert_equal mod, CharacterCreatorKata.ability_modifier(score),
+                   "Expected modifier for score #{score} to be #{mod}"
+    end
+  end
+end
+
+class ProficiencyBonusTest < Minitest::Test
+  def test_levels_1_through_4_have_bonus_2
+    (1..4).each do |level|
+      assert_equal 2, CharacterCreatorKata.proficiency_bonus(level)
+    end
+  end
+
+  def test_levels_5_through_8_have_bonus_3
+    (5..8).each do |level|
+      assert_equal 3, CharacterCreatorKata.proficiency_bonus(level)
+    end
+  end
+
+  def test_levels_9_through_12_have_bonus_4
+    (9..12).each do |level|
+      assert_equal 4, CharacterCreatorKata.proficiency_bonus(level)
+    end
+  end
+
+  def test_levels_13_through_16_have_bonus_5
+    (13..16).each do |level|
+      assert_equal 5, CharacterCreatorKata.proficiency_bonus(level)
+    end
+  end
+
+  def test_levels_17_through_20_have_bonus_6
+    (17..20).each do |level|
+      assert_equal 6, CharacterCreatorKata.proficiency_bonus(level)
+    end
+  end
+end
+
+class HitPointCalculationTest < Minitest::Test
+  def test_barbarian_level_1_con_mod_1
+    # d12: 12 + 1 + (1 * (7 + 1)) = 21
+    assert_equal 21, CharacterCreatorKata.calculate_hit_points("Barbarian", 1, 1)
+  end
+
+  def test_fighter_level_1_con_mod_2
+    # d10: 10 + 2 + (1 * (6 + 2)) = 20
+    assert_equal 20, CharacterCreatorKata.calculate_hit_points("Fighter", 1, 2)
+  end
+
+  def test_wizard_level_1_con_mod_0
+    # d6: 6 + 0 + (1 * (4 + 0)) = 10
+    assert_equal 10, CharacterCreatorKata.calculate_hit_points("Wizard", 1, 0)
+  end
+
+  def test_rogue_level_5_con_mod_1
+    # d8: 8 + 1 + (5 * (5 + 1)) = 39
+    assert_equal 39, CharacterCreatorKata.calculate_hit_points("Rogue", 5, 1)
+  end
+
+  def test_barbarian_level_10_con_mod_3
+    # d12: 12 + 3 + (10 * (7 + 3)) = 115
+    assert_equal 115, CharacterCreatorKata.calculate_hit_points("Barbarian", 10, 3)
+  end
+
+  def test_all_class_tiers_have_hit_dice
+    CharacterCreatorKata::CLASSES.each do |char_class|
+      assert CharacterCreatorKata::HIT_DICE.key?(char_class),
+             "#{char_class} should have a hit die entry"
+    end
+  end
+end
+
+class BackgroundBonusesTest < Minitest::Test
+  def test_acolyte_bonuses
+    bonuses = CharacterCreatorKata.background_bonuses("Acolyte")
+    assert_equal %w[Intelligence Wisdom Charisma], bonuses[:ability_bonuses]
+    assert_includes bonuses[:skill_proficiencies], "insight"
+    assert_includes bonuses[:skill_proficiencies], "religion"
+  end
+
+  def test_criminal_bonuses
+    bonuses = CharacterCreatorKata.background_bonuses("Criminal")
+    assert_equal %w[Dexterity Constitution Intelligence], bonuses[:ability_bonuses]
+    assert_includes bonuses[:skill_proficiencies], "sleight of hand"
+    assert_includes bonuses[:skill_proficiencies], "stealth"
+  end
+
+  def test_sage_bonuses
+    bonuses = CharacterCreatorKata.background_bonuses("Sage")
+    assert_equal %w[Constitution Intelligence Wisdom], bonuses[:ability_bonuses]
+    assert_includes bonuses[:skill_proficiencies], "arcana"
+    assert_includes bonuses[:skill_proficiencies], "history"
+  end
+
+  def test_soldier_bonuses
+    bonuses = CharacterCreatorKata.background_bonuses("Soldier")
+    assert_equal %w[Strength Dexterity Constitution], bonuses[:ability_bonuses]
+    assert_includes bonuses[:skill_proficiencies], "athletics"
+    assert_includes bonuses[:skill_proficiencies], "intimidation"
+  end
+
+  def test_unknown_background_returns_empty
+    bonuses = CharacterCreatorKata.background_bonuses("Unknown")
+    assert_empty bonuses[:ability_bonuses]
+    assert_empty bonuses[:skill_proficiencies]
+  end
+end
+
+class ClassProficienciesTest < Minitest::Test
+  def test_each_class_has_proficiency_list
+    CharacterCreatorKata::CLASSES.each do |char_class|
+      profs = CharacterCreatorKata.proficiency_by_class(char_class)
+      refute_empty profs, "#{char_class} should have skill proficiencies"
+    end
+  end
+
+  def test_barbarian_proficiencies
+    profs = CharacterCreatorKata.proficiency_by_class("Barbarian")
+    assert_includes profs, "athletics"
+    assert_includes profs, "intimidation"
+    assert_includes profs, "survival"
+  end
+
+  def test_unknown_class_returns_empty
+    assert_empty CharacterCreatorKata.proficiency_by_class("Artificer")
+  end
+end
+
+class SkillInitializationTest < Minitest::Test
+  def test_returns_all_18_skills
+    skills = CharacterCreatorKata.initialize_skills
+    assert_equal 18, skills.size
+  end
+
+  def test_all_skills_start_at_zero
+    skills = CharacterCreatorKata.initialize_skills
+    skills.each do |skill, value|
+      assert_equal 0, value, "#{skill} should start at 0"
+    end
+  end
+
+  def test_includes_expected_skills
+    skills = CharacterCreatorKata.initialize_skills
+    %w[athletics acrobatics stealth arcana perception].each do |skill|
+      assert skills.key?(skill), "Should include #{skill}"
+    end
+  end
+end
+
+class StatRollTest < Minitest::Test
+  def test_returns_standard_array
+    assert_equal [15, 14, 13, 12, 10, 8], CharacterCreatorKata.stat_roll
+  end
+
+  def test_returns_new_array_each_time
+    a = CharacterCreatorKata.stat_roll
+    b = CharacterCreatorKata.stat_roll
+    refute_same a, b
+  end
+end
+
+class CharacterTest < Minitest::Test
+  def setup
+    @ability_scores = CharacterCreatorKata::AbilityScores.new(
+      str: 15, dex: 14, con: 13, int: 12, wis: 10, cha: 8
+    )
+    @character = CharacterCreatorKata::Character.new(
+      name: "Adventurer", level: 1, species: "Dragonborn",
+      char_class: "Barbarian", background: "Acolyte",
+      ability_scores: @ability_scores, ac: 12,
+      proficiency_bonus: 2, hit_points: 21,
+      skills: CharacterCreatorKata.initialize_skills
+    )
+  end
+
+  def test_character_attributes
+    assert_equal "Adventurer", @character.name
+    assert_equal 1, @character.level
+    assert_equal "Dragonborn", @character.species
+    assert_equal "Barbarian", @character.char_class
+    assert_equal "Acolyte", @character.background
+    assert_equal 12, @character.ac
+    assert_equal 2, @character.proficiency_bonus
+    assert_equal 21, @character.hit_points
+  end
+
+  def test_character_ability_scores
+    assert_equal 15, @character.ability_scores.str
+    assert_equal 2, @character.ability_scores.str_mod
+    assert_equal 14, @character.ability_scores.dex
+    assert_equal 8, @character.ability_scores.cha
+    assert_equal(-1, @character.ability_scores.cha_mod)
+  end
+
+  def test_to_h_includes_all_fields
+    h = @character.to_h
+    assert_equal "Adventurer", h[:name]
+    assert_equal 1, h[:level]
+    assert_equal "Barbarian", h[:char_class]
+    assert_equal 15, h[:str]
+    assert_equal 2, h[:str_mod]
+    assert_equal 12, h[:ac]
+    assert_equal 21, h[:hit_points]
+    assert_equal 18, h[:skills].size
+  end
+end
+
+class CharacterCreationIntegrationTest < Minitest::Test
+  include CharacterCreatorKata
 
   def capture_stdout
     old_stdout = $stdout
     $stdout = StringIO.new
-    yield
-    $stdout.string
+    result = yield
+    [result, $stdout.string]
   ensure
     $stdout = old_stdout
   end
 
-  def test_character_creation_output_matches_expectation
-    output = capture_stdout { run_character_creation }
+  def test_main_returns_a_character
+    character, _output = capture_stdout { main }
 
-    expected_output = <<~OUTPUT
-      Welcome to the D&D Character Creator!
-      ========================================
-      Barbarian
-      Bard
-      Cleric
-      Druid
-      Fighter
-      Monk
-      Paladin
-      Ranger
-      Rogue
-      Sorcerer
-      Warlock
-      Wizard
-      What class is your character?
-      Barbarian
-      Dragonborn
-      Dwarf
-      Elf
-      Gnome
-      Goliath
-      Halfling
-      Human
-      Orc
-      Tiefling
-      What species is your character?
-      Dragonborn
-      Acolyte
-      Criminal
-      Sage
-      Soldier
-      What species is your character?
-      Acolyte
-      You rolled: [15, 14, 13, 12, 10, 8]
-      These are your stats:
-      15
-      14
-      13
-      12
-      10
-      8
-      Which number would you like to be your Strength stat?
-      15
-      These are your remaining stats:
-      14
-      13
-      12
-      10
-      8
-      Which number would you like to be your Dexterity stat?
-      14
-      These are your remaining stats:
-      13
-      12
-      10
-      8
-      Which number would you like to be your Constitution stat?
-      13
-      These are your remaining stats:
-      12
-      10
-      8
-      Which number would you like to be your Intelligence stat?
-      12
-      These are your remaining stats:
-      10
-      8
-      Which number would you like to be your Wisdom stat?
-      10
-      So your stats are  Strength: 15, Dexteriy: 14, Constitution: 13,
-          Intelligence: 12, Wisdom: 10, and Charisma: 8
-      Are you satisfied with this? (y/n)
-      y
-      
-      What is your character's name?
-      Adventurer
-      
-      ========================================
-      Character Created!
-      ========================================
-      {"@charname": "Adventurer"}
-      {"@level": 1}
-      {"@species": "Dragonborn"}
-      {"@class_of_char": "Barbarian"}
-      {"@background": "Acolyte"}
-      {"@str": 15}
-      {"@strmod": 2}
-      {"@dex": 14}
-      {"@dexmod": 2}
-      {"@con": 13}
-      {"@conmod": 1}
-      {"@int": 12}
-      {"@intmod": 1}
-      {"@wis": 10}
-      {"@wismod": 0}
-      {"@cha": 8}
-      {"@chamod": -1}
-      {"@ac": 12}
-      {"@prof": 2}
-      {"@hitpoints": 21}
-      {"@skills": {"athletics" => 0, "acrobatics" => 0, "sleight of hand" => 0, "stealth" => 0, "arcana" => 0, "history" => 0, "investigation" => 0, "nature" => 0, "religion" => 0, "animal handling" => 0, "insight" => 0, "medicine" => 0, "perception" => 0, "survival" => 0, "deception" => 0, "intimidation" => 0, "performance" => 0, "persuasion" => 0}}
-    OUTPUT
-
-    assert_equal normalize(expected_output), normalize(output)
+    assert_instance_of CharacterCreatorKata::Character, character
+    assert_equal "Adventurer", character.name
+    assert_equal 1, character.level
+    assert_equal "Barbarian", character.char_class
+    assert_equal "Dragonborn", character.species
+    assert_equal "Acolyte", character.background
+    assert_equal 15, character.ability_scores.str
+    assert_equal 14, character.ability_scores.dex
+    assert_equal 13, character.ability_scores.con
+    assert_equal 12, character.ability_scores.int
+    assert_equal 10, character.ability_scores.wis
+    assert_equal 8, character.ability_scores.cha
+    assert_equal 12, character.ac
+    assert_equal 2, character.proficiency_bonus
+    assert_equal 21, character.hit_points
+    assert_equal 18, character.skills.size
   end
 
-  def normalize(s)
-    s.lines.map(&:rstrip).join("\n") + "\n"
+  def test_main_output_includes_key_elements
+    _character, output = capture_stdout { main }
+
+    assert_includes output, "Welcome to the D&D Character Creator!"
+    assert_includes output, "Character Created!"
+    assert_includes output, "Adventurer"
+    assert_includes output, "Barbarian"
+    assert_includes output, "Dragonborn"
   end
 end
