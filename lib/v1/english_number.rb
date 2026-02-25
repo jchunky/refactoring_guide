c0e147d89dea3c66f9c22377740d89519269f431
Jason CheongKeeYou <jasonc@nulogy.com>
2026-02-25 00:51:09 -0500

    v1: refactor

--- lib/v1/english_number.rb
+++ lib/v1/english_number.rb
@@ -1,9 +1,35 @@
 # frozen_string_literal: true
 
 module EnglishNumberKata
-  ONES = %w[one two three four five six seven eight nine].freeze
-  TEENS = %w[eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen].freeze
-  TENS = %w[ten twenty thirty forty fifty sixty seventy eighty ninety].freeze
+  NUMBERS = {
+    1 => "one",
+    2 => "two",
+    3 => "three",
+    4 => "four",
+    5 => "five",
+    6 => "six",
+    7 => "seven",
+    8 => "eight",
+    9 => "nine",
+    10 => "ten",
+    11 => "eleven",
+    12 => "twelve",
+    13 => "thirteen",
+    14 => "fourteen",
+    15 => "fifteen",
+    16 => "sixteen",
+    17 => "seventeen",
+    18 => "eighteen",
+    19 => "nineteen",
+    20 => "twenty",
+    30 => "thirty",
+    40 => "forty",
+    50 => "fifty",
+    60 => "sixty",
+    70 => "seventy",
+    80 => "eighty",
+    90 => "ninety",
+  }.freeze
 
   def english_number(number)
     return "Please enter a number that isn't negative." if number < 0
@@ -14,24 +40,14 @@ module EnglishNumberKata
 
   private
 
-  def convert(number)
-    parts = []
-
-    hundreds, remainder = number.divmod(100)
-    if hundreds > 0
-      parts << "#{convert(hundreds)} hundred"
+  def convert(n)
+    case n
+    when (1..20)
+      NUMBERS[n]
+    when (21..99)
+      [NUMBERS[n.truncate(-1)], NUMBERS[n % 10]].compact.join("-")
+    when (100..)
+      [convert(n / 100), "hundred", convert(n % 100)].compact.join(" ")
     end
-
-    tens, ones = remainder.divmod(10)
-    if tens == 1 && ones > 0
-      parts << TEENS[ones - 1]
-    else
-      tens_and_ones = []
-      tens_and_ones << TENS[tens - 1] if tens > 0
-      tens_and_ones << ONES[ones - 1] if ones > 0
-      parts << tens_and_ones.join("-") unless tens_and_ones.empty?
-    end
-
-    parts.join(" ")
   end
 end
