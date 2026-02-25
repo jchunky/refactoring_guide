e6f20a20b32165dcdf566eb83b30243c4aa02492
Jason CheongKeeYou <jasonc@nulogy.com>
2026-02-25 00:30:46 -0500

    v1: refactor

--- lib/v1/medicine_clash.rb
+++ lib/v1/medicine_clash.rb
@@ -3,108 +3,35 @@
 require "date"
 
 class Integer
-  def day = days
+  def day = self
   def days = self
   def ago = Date.today - self
   def from_now = Date.today + self
 end
 
-class Date
-  def advance(options = {})
-    self + (options[:days] || 0)
-  end
-end
-
 module MedicineClashKata
-  class Prescription
-    attr_reader :dispense_date, :days_supply
-
-    def initialize(dispense_date: Date.today, days_supply: 30)
-      @dispense_date = dispense_date
-      @days_supply = days_supply
-    end
-
-    def <=>(other)
-      return -1 if dispense_date.nil?
-      return 1 if other.dispense_date.nil?
-
-      dispense_date <=> other.dispense_date
-    end
-
-    def completion_date
-      dispense_date + days_supply
-    end
-
-    def date_range
-      (dispense_date...completion_date).to_a
-    end
+  class Prescription < Data.define(:dispense_date, :days_supply)
+    def days_taken = (dispense_date...completion_date).to_a
+    def completion_date = dispense_date + days_supply
   end
 
-  class Medicine
-    attr_reader :name, :prescriptions
+  class Medicine < Struct.new(:name, :prescriptions)
+    def initialize(name) = super(name, [])
 
-    def initialize(name)
-      @name = name
-      @prescriptions = []
-    end
-
-    def most_recent_prescription
-      prescriptions.max_by(&:dispense_date)
-    end
-
-    def initial_dispense_date
-      prescriptions.min_by(&:dispense_date).dispense_date
-    end
-
-    def possession_end_date
-      most_recent_prescription.completion_date
-    end
-
-    def possession_effective_end_date
-      [possession_end_date, Date.today].min
-    end
-
-    def dates_prescribed_in_effective_range(day_count)
-      return [] if prescriptions.empty?
-
-      effective_range = effective_range_for(day_count)
-      all_prescribed_dates(day_count).select { |d| effective_range.include?(d) }
-    end
-
-    private
-
-    def effective_range_for(day_count)
-      start_date = [possession_effective_end_date - day_count, initial_dispense_date].max
-      start_date...possession_effective_end_date
-    end
-
-    def all_prescribed_dates(day_count)
-      effective_range = effective_range_for(day_count)
-
-      prescriptions
-        .select { |p| overlaps_range?(p, effective_range) }
-        .flat_map(&:date_range)
-        .uniq
-    end
-
-    def overlaps_range?(prescription, range)
-      range.include?(prescription.dispense_date) || range.include?(prescription.completion_date)
-    end
+    def days_taken = prescriptions.flat_map(&:days_taken).uniq
   end
 
-  class Patient
-    attr_reader :medicines
-
-    def initialize
-      @medicines = []
-    end
+  class Patient < Struct.new(:medicines)
+    def initialize = super([])
 
     def clash(medicine_names, days_back)
-      relevant_medicines = medicines.select { |m| medicine_names.include?(m.name) }
-      return [] if relevant_medicines.empty?
-
-      date_sets = relevant_medicines.map { |m| m.dates_prescribed_in_effective_range(days_back) }
-      date_sets.reduce(:&)
+      today = Date.today
+      effective_range = ((today - days_back)...today)
+      medicines
+        .select { |medicine| medicine_names.include?(medicine.name) }
+        .map(&:days_taken)
+        .reduce(&:intersection)
+        .select { |date| effective_range.include?(date) }
     end
   end
 end
