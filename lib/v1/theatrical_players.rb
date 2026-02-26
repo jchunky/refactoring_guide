cbd9beac1c1bd226c5ff6da0fd2af26c69b6c78c
Jason CheongKeeYou <jasonc@nulogy.com>
2026-02-25 22:56:10 -0500

    v1: refactor

--- lib/v1/theatrical_players.rb
+++ lib/v1/theatrical_players.rb
@@ -1,56 +1,65 @@
 # frozen_string_literal: true
 
 module TheatricalPlayersKata
-  def statement(invoice, plays)
-    performances = invoice["performances"].map { |perf| enrich(perf, plays) }
-    total_amount = performances.sum { |p| p[:amount] }
-    total_credits = performances.sum { |p| p[:credits] }
-
-    lines = ["Statement for #{invoice["customer"]}"]
-    performances.each do |perf|
-      lines << " #{perf[:name]}: #{format_currency(perf[:amount])} (#{perf[:audience]} seats)"
-    end
-    lines << "Amount owed is #{format_currency(total_amount)}"
-    lines << "You earned #{total_credits} credits"
-    "#{lines.join("\n")}\n"
-  end
-
-  private
-
-  def enrich(performance, plays)
-    play = plays[performance["playID"]]
-    audience = performance["audience"]
-
-    {
-      name: play["name"],
-      audience: audience,
-      amount: amount_for(play["type"], audience),
-      credits: credits_for(play["type"], audience),
-    }
-  end
-
-  def amount_for(type, audience)
-    case type
-    when "tragedy"
-      base = 40_000
-      base += 1_000 * (audience - 30) if audience > 30
-      base
-    when "comedy"
-      base = 30_000
-      base += 10_000 + (500 * (audience - 20)) if audience > 20
-      base + (300 * audience)
-    else
-      raise "unknown type: #{type}"
+  class Performance < Struct.new(:play_name, :seat_count)
+    def self.build(play_type, play_name, seat_count)
+      Object.const_get(play_type.capitalize).new(play_name, seat_count)
+    rescue NameError
+      raise "unknown type: #{play_type}"
     end
   end
 
-  def credits_for(type, audience)
-    credits = [audience - 30, 0].max
-    credits += (audience / 5).floor if type == "comedy"
-    credits
+  class Tragedy < Performance
+    def price
+      result = 40_000
+      result += 1_000 * (seat_count - 30) if seat_count > 30
+      result
+    end
+
+    def credits = (seat_count - 30).clamp(0..)
   end
 
-  def format_currency(amount_in_cents)
-    "$#{format("%.2f", amount_in_cents / 100.0)}"
+  class Comedy < Performance
+    def price
+      result = 30_000 + (300 * seat_count)
+      result += 10_000 + (500 * (seat_count - 20)) if seat_count > 20
+      result
+    end
+
+    def credits
+      result = (seat_count - 30).clamp(0..)
+      result += (seat_count / 5).floor
+      result
+    end
   end
+
+  class Statement < Struct.new(:customer, :performances)
+    def self.build(invoice, plays)
+      performances =
+        invoice["performances"].map do |perf|
+          play = plays[perf["playID"]]
+          Performance.build(play["type"], play["name"], perf["audience"])
+        end
+      new(invoice["customer"], performances)
+    end
+
+    def to_s
+      [
+        "Statement for #{customer}",
+        performances.map do |perf|
+          format(" %s: %s (%s seats)", perf.play_name, usd(perf.price), perf.seat_count)
+        end,
+        "Amount owed is #{usd(total_price)}",
+        "You earned #{total_credits} credits",
+      ].join("\n").concat("\n")
+    end
+
+    private
+
+    def total_price = performances.sum(&:price)
+    def total_credits = performances.sum(&:credits)
+    def usd(cents) = format("$%.2f", cents / 100.0)
+  end
+
+  def statement(invoice, plays) = Statement.build(invoice, plays).to_s
 end
